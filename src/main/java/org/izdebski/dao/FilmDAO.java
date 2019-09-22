b507719788c6a9a442f86b3823895c6c9db3a0d3
ivan <iizdepskyy112@gmail.com>
2019-09-22 10:50:12 +0300

    Filmography Part 4

---
+++ src/main/java/org/izdebski/dao/FilmDAO.java
@@ -0,0 +1,13 @@
+package org.izdebski.dao;
+
+import org.izdebski.model.Film;
+
+import java.util.List;
+
+public interface FilmDAO {
+    List<Film> allFilms();
+    void add(Film film);
+    void delete(Film film);
+    void edit(Film film);
+    Film getById(int id);
+}
---
+++ src/main/java/org/izdebski/service/FilmService.java
@@ -0,0 +1,13 @@
+package org.izdebski.service;
+
+import org.izdebski.model.Film;
+
+import java.util.List;
+
+public interface FilmService {
+    List<Film> allFilms();
+    void add(Film film);
+    void delete(Film film);
+    void edit(Film film);
+    Film getById(int id);
+}
